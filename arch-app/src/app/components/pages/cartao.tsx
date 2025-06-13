008353c14866d25a075a6895407bd60970d11fdc
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2025-06-13 09:24:44 -0300

    update

--- arch-app/src/app/components/pages/cartao.tsx
+++ arch-app/src/app/components/pages/cartao.tsx
@@ -4,7 +4,7 @@ import { DrawerComponent } from "./section/drawer";
 
 export default function Cartao() {
   return (
-    <div className="w-[90%] max-w-sm bg-[#EFE8D899] p-6 rounded-lg shadow-lg text-center backdrop-blur-md">
+    <div className="w-[90%] max-w-sm bg-[#EFE8D899] p-6 text-center ">
       <Redes />
       <Perfil />
       <DrawerComponent />
